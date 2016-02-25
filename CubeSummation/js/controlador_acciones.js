6024ca3f43bbd729d7ddfbc1349f32f66596d2c2
darkdrei <darkdrei88@gmail.com>
2016-02-25 00:07:34 -0500

    Componente encargado de la interraccion para el cube summation, se establecen las dependencias con los escrip de core

---
+++ CubeSummation/js/controlador_acciones.js
@@ -0,0 +1,11 @@
+var core;
+$(document).on('ready',function(){
+	$('.accion').on('click',function(){
+		return false;
+	});
+	$('.accion').on('click',function(){
+		if(core == undefined){
+			$('#tablero').append('Digite el valor pata T : ');
+		}
+	});
+});
